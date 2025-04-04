9010533bdb1e5aed573eadd2b4a61343d27dedee
M3RLIN01 <changeme@example.com>
2025-04-04 16:19:05 +0000

    Codigo listo

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("Calculadora")
-print("")
+# Menu principal de la calculadora
+from funciones import suma, resta, multiplicacion, division
+
+while True:
+    """
+    1) Suma
+    2) Resta
+    3) Multiplicacion
+    4) Division
+    5) Salir
+    """
