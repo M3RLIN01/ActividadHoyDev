4f469e992a956108b9d03da8a4a50cc4a38af4cc
sebavega7 <changeme@example.com>
2025-04-04 16:35:05 +0000

    error en if

--- main.py
+++ main.py
@@ -10,27 +10,26 @@ while True:
     4) Division
     5) Salir
     """
+    opcion = int(input("Seleccione su opcion: "))
 
-opcion = int(input("Seleccione su opcion: "))
-
-if opcion == 1:
-    num1 = int(input("Ingrese el primer numero: "))
-    num2 = int(input("Ingrese el segundo numero: "))
-    suma(num1, num2)
-elif opcion == 2:
-    num1 = int(input("Ingrese el primer numero: "))
-    num2 = int(input("Ingrese el segundo numero: "))
-    resta(num1, num2)
-elif opcion == 3:
-    num1 = int(input("Ingrese el primer numero: "))
-    num2 = int(input("Ingrese el segundo numero: "))
-    multiplicacion(num1, num2)
-elif opcion == 4:
-    num1 = int(input("Ingrese el primer numero: "))
-    num2 = int(input("Ingrese el segundo numero: "))
-    division(num1, num2)
-elif opcion == 5:
-    print("Hasta luego")
-    break
-else:
-    print("opcion erronea, vuelva a seleccionar")
+    if opcion == 1:
+        num1 = int(input("Ingrese el primer numero: "))
+        num2 = int(input("Ingrese el segundo numero: "))
+        suma(num1, num2)
+    elif opcion == 2:
+        num1 = int(input("Ingrese el primer numero: "))
+        num2 = int(input("Ingrese el segundo numero: "))
+        resta(num1, num2)
+    elif opcion == 3:
+        num1 = int(input("Ingrese el primer numero: "))
+        num2 = int(input("Ingrese el segundo numero: "))
+        multiplicacion(num1, num2)
+    elif opcion == 4:
+        num1 = int(input("Ingrese el primer numero: "))
+        num2 = int(input("Ingrese el segundo numero: "))
+        division(num1, num2)
+    elif opcion == 5:
+        print("Hasta luego")
+        break
+    else:
+        print("opcion erronea, vuelva a seleccionar")
